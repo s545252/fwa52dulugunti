dfeda5114df2f8f9b434daace650b4b41853ca3e
s545252 <s545252@nwmissouri.edu>
2021-10-27 17:33:39 -0500

    added code to the file

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Madhumitha Reddy' });
+  res.render('mydata', { title: 'Madhumitha Reddy' });
 });
 
 module.exports = router;
